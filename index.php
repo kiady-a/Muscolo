3f4fab4d6f6ddcaeafbe1cfde130d113a9899ed7
PEREIRA MENDES CRISTIANO <cristiano.prrmn@eduge.ch>
2018-02-20 11:33:02 +0100

    debut vue principale

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+//require_once 'model/fonctions.php';
+
+include 'views/game.php';
+
+?>
---
+++ views/game.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Muscolo</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" href="" /><!-- icon -->
+        <style>
+            html{
+              background-color: #001f54;
+              color : #fefcfb;
+            }
+        </style>
+    </head>
+    <body>
+      <noscript><p>Please enable javascript for this site to work properly</p><style>
+          div {
+              display: none;
+          }
+      </style></noscript>
+      <h1>Welcome here !</h1>
+      </body>
+</html>
