7a0c730aaa9b8a79fc12d0ea899478c03f8b321b
CristianoPM <35951463+CristianoPM@users.noreply.github.com>
2018-03-27 11:22:54 +0200

    update img

--- views/game.php
+++ views/game.php
@@ -170,14 +170,14 @@
                             <div class="modal-content">
                                 <div class="modal-header">
                                     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                                    <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+                                    <h4 class="modal-title" id="myModalLabel">Oops...</h4>
                                 </div>
                                 <div class="modal-body">
-                                    ...
+                                    You need to login to save your group
                                 </div>
                                 <div class="modal-footer">
-                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                                    <button type="button" class="btn btn-primary">Save changes</button>
+                                    <a href="./c_login.php" class="btn btn-primary">Login</a>
+                                    <a href="./c_login.php?register=register" class="btn btn-primary">Create an account</a>
                                 </div>
                             </div>
                         </div>
